9070d473769b7931e12f4e52a0732182e1ab4924
aminaanwar <aanwar2@ggc.edu>
2020-10-15 00:26:54 -0400

    modified Amina Branch and updating it

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import FrontPage from "../views/FrontPage.vue";
-import About from "../views/About.vue";
+
 
 Vue.use(VueRouter);
 
@@ -20,23 +20,6 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views//Players.vue")
   },
-  {
-    path: "//About",
-    name: "About",
-    component: About
-  },
-  {
-    path: "//Login",
-    name: "Login",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views//Login.vue")
-  },
- 
-
-
 
 ];
 
