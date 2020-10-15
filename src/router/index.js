ac20c57f80e6e27c4be9b803a45de41b524e71e4
aminaanwar <aanwar2@ggc.edu>
2020-10-15 00:33:00 -0400

    updated index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import FrontPage from "../views/FrontPage.vue";
-
+import About from "../views/About.vue";
+import Clans from "../views/Clans.vue";
+import Community from "../views/Community.vue";
+import Servers from "../views/Servers.vue";
 
 Vue.use(VueRouter);
 
@@ -20,6 +23,38 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views//Players.vue")
   },
+  {
+    path: "//About",
+    name: "About",
+    component: About
+  },
+  {
+    path: "//Clans",
+    name: "Clans",
+    component: Clans
+  },
+  {
+    path: "//Community",
+    name: "Community",
+    component: Community
+  },
+  {
+    path: "//Servers",
+    name: "Servers",
+    component: Servers
+  },
+  {
+    path: "//Login",
+    name: "Login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views//Login.vue")
+  },
+ 
+
+
 
 ];
 
